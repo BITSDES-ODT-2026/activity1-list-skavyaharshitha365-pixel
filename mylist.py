ff2fb7c81923f52aa3db47a94672ca667068f3ac
Kavya Harshitha <skavyaharshitha365@gmail.com>
2026-02-17 11:32:50 +0530

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -2,13 +2,14 @@ from machine import Pin,PWM
 import time
 
 l1,l2,l3,l4= Pin(5,Pin.OUT),Pin(4,Pin.OUT),Pin(13,Pin.OUT),Pin(1,Pin.OUT)
-l=[l1,l2,l3,l4]
+l=[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
 
 while True:
     for i in l:
-        i.value(1)
-        time.sleep(0.4)
-        i.value(0)
-        time.sleep(0.05)
-
-#Create any list
+        l1.value(i[0])
+        l2.value(i[1])
+        l3.value(i[2])
+        l4.value(i[3])
+        time.sleep(0.2)
+        
