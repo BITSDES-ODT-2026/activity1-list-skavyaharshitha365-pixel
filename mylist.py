6bfba940ecb71aa12c408267aaefd9cca10ac320
Kavya Harshitha <skavyaharshitha365@gmail.com>
2026-02-17 10:45:07 +0530

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -1,1 +1,14 @@
+from machine import Pin,PWM
+import time
+
+l1,l2,l3,l4= Pin(5,Pin.OUT),Pin(4,Pin.OUT),Pin(13,Pin.OUT),Pin(1,Pin.OUT)
+l=[l1,l2,l3,l4]
+
+while True:
+    for i in l:
+        i.value(1)
+        time.sleep(0.4)
+        i.value(0)
+        time.sleep(0.05)
+
 #Create any list
